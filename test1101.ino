c424deec47440814bfa1763d3beab3c8c6d662d5
passivemonster <johnkaramchand1@gmail.com>
2019-10-19 21:40:55 +0530

    first

---
+++ test1101.ino
@@ -0,0 +1,310 @@
+#include <DHTesp.h>         // DHT for ESP32 library
+#include <WiFi.h>           // WiFi control for ESP32
+#include <ThingsBoard.h>    // ThingsBoard SDK
+
+int a[18] = {2,4,5,12,13,14,15,18,19,21,22,23,25,26,32,33,34,35};
+// Helper macro to calculate array size
+#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+
+// WiFi access point
+#define WIFI_AP_NAME        "free"
+// WiFi password
+#define WIFI_PASSWORD       "freefree"
+
+// See https://thingsboard.io/docs/getting-started-guides/helloworld/ 
+// to understand how to obtain an access token
+#define TOKEN               "john_is_token"
+// ThingsBoard server instance.
+#define THINGSBOARD_SERVER  "157.245.102.35"
+
+// Baud rate for debug serial
+#define SERIAL_DEBUG_BAUD    115200
+
+
+struct esppins{
+    int pin;
+    int state;
+  };
+
+typedef struct esppins ESPPIN;
+
+ESPPIN esppin[18];
+
+
+
+ 
+
+// Initialize ThingsBoard client
+WiFiClient espClient;
+// Initialize ThingsBoard instance
+ThingsBoard tb(espClient);
+// the Wifi radio's status
+int status = WL_IDLE_STATUS;
+
+// Array with LEDs that should be lit up one by one
+uint8_t leds_cycling[] = { 2 };
+// Array with LEDs that should be controlled from ThingsBoard, one by one
+uint8_t leds_control[] = { 2 };
+
+// DHT object
+DHTesp dht;
+// ESP32 pin used to query DHT22
+#define DHT_PIN 33
+
+// Main application loop delay
+int quant = 20;
+
+// Initial period of LED cycling.
+int led_delay = 1000;
+// Period of sending a temperature/humidity data.
+int send_delay = 2000;
+
+// Time passed after LED was turned ON, milliseconds.
+int led_passed = 0;
+// Time passed after temperature/humidity data was sent, milliseconds.
+int send_passed = 0;
+
+// Set to true if application is subscribed for the RPC messages.
+bool subscribed = false;
+// LED number that is currenlty ON.
+int current_led = 0;
+int active = 0 ;
+// Processes function for RPC call "setValue"
+// RPC_Data is a JSON variant, that can be queried using operator[]
+// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
+RPC_Response processDelayChange(const RPC_Data &data)
+{
+  Serial.println("Received the set delay RPC method");
+
+  // Process data
+
+  active = data;
+
+  Serial.print("Set new delay: ");
+  Serial.println(active);
+  digitalWrite(2, active);
+
+  return RPC_Response("active", active);
+}
+
+// Processes function for RPC call "getValue"
+// RPC_Data is a JSON variant, that can be queried using operator[]
+// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
+
+RPC_Response processGetDelay(const RPC_Data &data)
+{
+  Serial.println("Received the get value method");
+
+  return RPC_Response("active", active);
+}
+
+
+// Processes function for RPC call "setGpioStatus"
+// RPC_Data is a JSON variant, that can be queried using operator[]
+// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
+RPC_Response processSetGpioState(const RPC_Data &data)
+{
+  Serial.println("Received the set GPIO RPC method");
+
+ // Serial.print(data["pin"]);
+  int pin = data["pin"];
+  active = data["enabled"];
+
+  //if (pin < COUNT_OF(leds_control)) {
+    Serial.print("Setting LED ");
+    Serial.print(pin);
+    Serial.print(" to state ");
+    Serial.println(active);
+
+    // To get index of the pin
+    int j;
+    for(j=0;j<18;j++){
+      if(esppin[j] == pin){
+        break;
+        }
+      }
+
+     // setting strct status
+     esppin[j].status = active;
+    
+
+    digitalWrite(pin, active);
+  
+
+  return RPC_Response(data["pin"], (bool)data["enabled"]);
+}
+// Processes function for RPC call "getGpioStatus"
+// RPC_Data is a JSON variant, that can be queried using operator[]
+// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
+RPC_Response processGetGpioState(const RPC_Data &data)
+{
+  Serial.println("Received the get GPIO RPC method");
+
+   int pin = data["pin"];
+  //active = 1;
+/*
+  if (pin < COUNT_OF(leds_control)) {
+    Serial.print("Setting LED ");
+    Serial.print(pin);
+    Serial.print(" to state ");
+    Serial.println(enabled);
+
+    digitalWrite(pin, enabled);
+  }*/
+  
+    //Serial.print("Getting LED ");
+    //Serial.print(pin);
+    char* str ;
+    //Serial.print(" whose state is");
+    //Serial.println(active);
+    //StaticJsonDocument<256> doc;
+    //doc["pin"] = true ;
+    //serializeJson(doc, str);
+    //char buffer[1024];
+    //int a = snprintf(buffer, sizeof(buffer), "{\"pin\ ": %d}", (bool)active);
+    
+  return RPC_Response("data", active);
+}
+
+// RPC handlers
+RPC_Callback callbacks[] = {
+  { "setValue",         processDelayChange },
+  { "getValue",         processGetDelay },
+  { "setGpioStatus",    processSetGpioState },
+  { "getGpioStatus",    processGetGpioState },
+};
+
+// Setup an application
+void setup() {
+  
+  // Initialize serial for debugging
+  Serial.begin(SERIAL_DEBUG_BAUD);
+  WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
+  InitWiFi();
+
+  // Pinconfig
+
+  for (size_t i = 0; i < COUNT_OF(leds_cycling); ++i) {
+    pinMode(leds_cycling[i], OUTPUT);
+  }
+
+  for (size_t i = 0; i < COUNT_OF(leds_control); ++i) {
+    pinMode(leds_control[i], OUTPUT);
+  }
+
+  //setup for the pins  
+  int z;
+  for (z=0;z<18;z++){
+    esppin[z].pin = a[z];
+    esppin[z].state = 0;
+    }
+
+  // Initialize temperature sensor
+  dht.setup(DHT_PIN, DHTesp::DHT22);
+}
+
+// Main application loop
+void loop() {
+  
+  Serial.println(esppin[17].pin);
+  delay(quant);
+
+  led_passed += quant;
+  send_passed += quant;
+
+  // Check if next LED should be lit up
+  if (led_passed > led_delay) {
+    // Turn off current LED
+    digitalWrite(leds_cycling[2], LOW);
+    led_passed = 0;
+    current_led = current_led >= 2 ? 0 : (current_led + 1);
+    // Turn on next LED in a row
+    digitalWrite(leds_cycling[2], HIGH);
+  }
+
+  // Reconnect to WiFi, if needed
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnect();
+    return;
+  }
+
+  // Reconnect to ThingsBoard, if needed
+  if (!tb.connected()) {
+    subscribed = false;
+
+    // Connect to the ThingsBoard
+    Serial.print("Connecting to: ");
+    Serial.print(THINGSBOARD_SERVER);
+    Serial.print(" with token ");
+    Serial.println(TOKEN);
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+      Serial.println("Failed to connect");
+      return;
+    }
+  }
+
+  // Subscribe for RPC, if needed
+  if (!subscribed) {
+    Serial.println("Subscribing for RPC...");
+
+    // Perform a subscription. All consequent data processing will happen in
+    // callbacks as denoted by callbacks[] array.
+   /* if (!tb.RPC_Subscribe(callbacks, COUNT_OF(callbacks))) {
+      Serial.println("Failed to subscribe for RPC");
+      return;
+    }*/
+
+    tb.RPC_Subscribe(callbacks, COUNT_OF(callbacks)) ;
+
+    Serial.println("Subscribe done");
+    subscribed = true;
+  }
+
+  // Check if it is a time to send DHT22 temperature and humidity
+  if (send_passed > send_delay) {
+    //Serial.println("Sending data...");
+
+    // Uploads new telemetry to ThingsBoard using MQTT. 
+    // See https://thingsboard.io/docs/reference/mqtt-api/#telemetry-upload-api 
+    // for more details
+
+    TempAndHumidity lastValues = dht.getTempAndHumidity();    
+    if (isnan(lastValues.humidity) || isnan(lastValues.temperature)) {
+      //Serial.println("Failed to read from DHT sensor!");
+    } else {
+      //tb.sendTelemetryFloat("temperature", lastValues.temperature);
+     // tb.sendTelemetryFloat("humidity", lastValues.humidity);
+    }
+
+    send_passed = 0;
+  }
+
+  // Process messages
+  tb.loop();
+}
+
+void InitWiFi()
+{
+  Serial.println("Connecting to AP ...");
+  // attempt to connect to WiFi network
+
+  WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to AP");
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("Connected to AP");
+  }
+}
